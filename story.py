a2adbb12e32fc4ce03680f35b0f3d2e1862d0443
Laron Walker <233176084+lwalke55@users.noreply.github.com>
2025-09-24 13:39:50 -0400

    added pathway for user to go into city or not and different outcomes for the battle

--- story.py
+++ story.py
@@ -13,6 +13,18 @@ def left_path():
     print("after picking up the sword you travel further down the path and find a village..")
     print("do you go in the village? (yes/no)")
     choice = input("do you go in the village? (yes/no): ").strip().lower()
+    if choice == "yes":
+        print("going into the village, you find armor and a quest log to slay a dragon")
+        print("you delve deeper into the path and go inside a cave:")
+        print("you encounter a dragon, controlled by an evil wizard!")
+        print("with armor and preperation, the fight was a cinch and they both went down without a sweat.")
+    elif choice == "no":
+        print("you avoid the village, and continue to walk down the path.")
+        print("you delve deeper into the path and go inside a cave:")
+        print("you encounter a dragon, controlled by an evil wizard!")
+        print("as you have no armor, the battle was tough but you prevailed and were able to slay the wizard and its dragon.")
+    print("at the end of the day, you keep the dragons egg you found after defeating it and raise it as your own.")
+
 def right_path():
     print("You walk right and encounter a talking squirrel who challenges you to a duel.")
 
