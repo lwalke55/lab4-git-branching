23fbba96aba614a18149c5e56d764f8b802dd24b
Laron Walker <233176084+lwalke55@users.noreply.github.com>
2025-09-26 23:44:36 +0000

    added print statements for each choice

--- story.py
+++ story.py
@@ -16,6 +16,19 @@ def right_path():
     print("the squirrel went down easy, but a wizard gives way behind the trees")
     print("the wizard asks, 'would you like to know the ways of wizardy?' ")
     choice = input("would you like to know the ways of wizardry? (yes/no): ").strip().lower()
+    if choice == "yes":
+        print("the wizard, instead of teaching you, takes possession of your body and your now only a backseat driver to your own body...")
+        print("the wizard tames a dragon, and teleports both the dragon and the body to a new place, in a cave.")
+        print("someone.. similar walks in. you start to realize its you but from a different universe.")
+        print("the wizard triumphs over this alternate reality of you, and takes over the world")
+    elif choice == "no":
+        print("you say no, and take the wizards spell book and run deeper into the forest")
+        print("distracted from reading the book, a dragon sneaks up on you!")
+        print("not knowing what a single spell does, you ramble off a couple lines until you get the dragon to calm down.. it almost seems as if you tamed it")
+        print("you try a 'teleportation' spell of sorts and it transports you and the dragon to a cave.. odd")
+        print("you hear someone approaching the cave, thinking the wizard has sent someone for you.")
+        print("you waste no time and slay the person before, but as you come to remove their helmet, you realize.. its you")
+    print("as you live the rest of your life, you think about what happened and if anything makes sense anymore..")
 
 if __name__ == "__main__":
     intro()
